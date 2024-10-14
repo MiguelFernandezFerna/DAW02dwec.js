c1a2375a5a3acc0f73eb58364613328f57fafe69
Miguel Fernandez Fernandez <miguel.ferfer@educa.jcyl.es>
2024-10-14 10:57:54 +0200

    Matriz traspuesta
    Hemos añadido que se imprima la matriz original sin trasponer

--- U3/diagonal_principal.js
+++ U3/diagonal_principal.js
@@ -1,12 +1,8 @@
-let mat = [[1,2,3],[4,5,6],[7,8,9]];
+let mat = [[1,2,3],
+            [4,5,6],
+            [7,8,9]];
 
-// for (let i = 0; i < mat.length; i++) {
-//     for (let j = 0; j < mat.length; j++) {
-//         if (i==j) {
-//             console.log(mat[i][j]);
-//         }
-//     }
-// }
+console.log(mat);
 
 function transponer(matriz) {
     let arr = [];
