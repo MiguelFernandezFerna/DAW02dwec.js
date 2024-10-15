c7b5d7eddf60852bf42a5788e8b8fb6a6a9ddb72
Miguel Fernandez <miguel.ferfer@educa.jcyl.es>
2024-10-15 16:48:06 +0200

    modificacion de texto, no relevante

--- U3/3enRaya.js
+++ U3/3enRaya.js
@@ -2,7 +2,7 @@ let tablero=[[1,2,3],
         [4,5,6],
         [7,8,9]];
 
-elegir = prompt("Dime la coordenada X que deseas poner");
+elegir = prompt("Dime la coordenada que deseas poner");
 for (let X = 0; X < tablero.length; X++) {
     for (let Y = 0; Y < tablero.length; Y++) {
         if (tablero[X][Y]==elegir&&tablero[X][Y]=="X"||tablero[X][Y]=="O") {
