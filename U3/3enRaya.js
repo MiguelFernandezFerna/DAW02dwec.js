4835fa6abb1d652fad8f36efdcebdc1b4aeb4ec0
Miguel Fernandez <miguel.ferfer@educa.jcyl.es>
2024-10-15 16:45:31 +0200

    Novedades del programa del 3 en 3enRaya
    Ahora para un turno el programa funciona, el
    problema es hacer que vaya hasta ciertos puntos en
    los que haya que meterle booleanos y un bucle
    while con diversas salidas

--- U3/3enRaya.js
+++ U3/3enRaya.js
@@ -1,28 +1,31 @@
 let tablero=[[1,2,3],
         [4,5,6],
         [7,8,9]];
+
 elegir = prompt("Dime la coordenada X que deseas poner");
-
 for (let X = 0; X < tablero.length; X++) {
-
     for (let Y = 0; Y < tablero.length; Y++) {
-        if (tablero[X][Y]==elegir) {
+        if (tablero[X][Y]==elegir&&tablero[X][Y]=="X"||tablero[X][Y]=="O") {
+            alert("La posición está ocupada, pierde turno");
+        } 
+        if (tablero[X][Y]==elegir&&tablero[X][Y]!="X"&&tablero[X][Y]!="O") {
             tablero[X][Y]="X";
         }
     }
 }
-console.log(tablero);
-elegir = prompt("Dime la coordenada que deseas poner");
 
+elegir2 = prompt("Dime la coordenada que deseas poner");
 for (let X = 0; X < tablero.length; X++) {
     for (let Y = 0; Y < tablero.length; Y++) {
-        if (tablero[X][Y]==elegir) {
+        if (tablero[X][Y]==elegir2&&tablero[X][Y]=="X"||tablero[X][Y]=="O") {
+            alert("La posición está ocupada, pierde turno");
+        } 
+        if (tablero[X][Y]==elegir2&&tablero[X][Y]!="X"&&tablero[X][Y]!="O") {
             tablero[X][Y]="O";
         }
     }
 }
-
-    console.log(tablero);
-        
+alert(tablero);
+console.log(tablero);
     
     
