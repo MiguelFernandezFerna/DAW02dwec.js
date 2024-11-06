53512fa00163d0152c8f64c0e8e3f68940669f3b
Miguel Fernandez <miguel.ferfer@educa.jcyl.es>
2024-11-06 16:41:45 +0100

    Modificacion de comentarios

--- U3/Arr_repetidos.js
+++ U3/Arr_repetidos.js
@@ -9,11 +9,11 @@ function repetidos(matriz) {
     for (let i = 0; i < matriz.length; i++) {
         let cont = 0;
         aux = matriz[i];
-        //si esta es la ultimaaparicion se mete dentro y empieza un cbucle de 0 para calcular todas las apariciones y sumarlas en un contador
+        //si esta es la ultima aparicion se mete dentro y recorremos el array de 0 para calcular todas las apariciones y sumarlas en un contador
         if (aux!=matriz[i+1]) {
             for (let j = 0; j < matriz.length; j++) {
                 num = matriz[i];
-                //si la posicion es igual al número que hemos cogido del primer recorrido del array sumamos
+                //si el número de la posicion es igual al número que hemos cogido del primer recorrido del array sumamos 1 al contador
             if (num==matriz[j]) {
                 cont++;
             }
