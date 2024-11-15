11a5a8f16c7de27e36cc5f5c87982cb596369dca
Miguel Fernandez <miguel.ferfer@educa.jcyl.es>
2024-11-15 16:41:56 +0100

    Tabla objetos, ahora funciona

--- U3/objetos/app.js
+++ U3/objetos/app.js
@@ -1,101 +1,103 @@
-"use strict";
+"use strict"
 
-const CABECERAS_VALIDAS = ['Nombre','Apellido','Año Nacimiento','Sueldo'];
 
-class Empleado {
+class empleado {
     #nombre;
-    #apellido;
+    #apellidos;
     #nacimiento;
     #sueldo;
-
-    constructor(nombre, apellido, nacimiento, sueldo) {
+    constructor(nombre, apellidos, nacimiento, sueldo) {
         this.#nombre = nombre;
-        this.#apellido = apellido;
-        this.#nacimiento=nacimiento;
-        this.#sueldo=sueldo;
+        this.#apellidos = apellidos;
+        this.#nacimiento = nacimiento;
+        this.#sueldo = sueldo;
     }
+    get nombre() {
+        return this.#nombre;
+    }
+
+    get apellidos() {
+        return this.#apellidos;
+    }
+
+    get nacimiento() {
+        return this.#nacimiento;
+    }
+
+    get sueldo() {
+        return this.#sueldo;
+    }
+
     toString(){
         return `<tr>
-            <td>${this.#nombre}</td>
-            <td>${this.#apellido}</td>
-            <td>${this.#nacimiento}</td>
-            <td>${this.#sueldo}</td>
-        </tr>`;
+                <td>${this.#nombre}</td>
+                <td> ${this.#apellidos}</td>
+                <td> ${this.#nacimiento}</td>
+                <td> ${this.#sueldo}</td>
+                </tr>`;
     }
-    get nombre(){return this.#nombre};
-    set nombre(nombre){this.#nombre=nombre};
 
-    get apellido(){return this.#apellido};
-    set apellido(apellido){this.#apellido=apellido};
-
-    get nacimiento(){return this.#nacimiento};
-    set nacimiento(nacimiento){this.#nacimiento=nacimiento};
-
-    get sueldo(){return this.#sueldo};
-    set sueldo(sueldo){this.#sueldo=sueldo};
 }
-
-
 
 let empleados = [
-    new Empleado("Paco","Fiestas", 1997,33000),
-    new Empleado("Chindas","Viento", 2001,27000),
-    new Empleado("Juan","Cruz", 1772,38000),
-    new Empleado("Misty","Perez", 1991,45000),
-    new Empleado("Joan","Laporta", 1987,12000),
-    new Empleado("Sabrina","Carpenter", 2001,23000),
-    new Empleado("Eulalio","Fernández", 1999,56000),
-];
+    new empleado("Paco","Fiestas",1997,27000),
+    new empleado("Chindas","Vinto",2001,33000),
+    new empleado("Juan","Cruz",2005,38000),
+    new empleado("Paco","Merte",1990,50000),
+    new empleado("Sabrina","Carpenter",2000,100000),
+    new empleado("Eulerio","Rodolfo",1925,12000),
+    new empleado("Ana","de Alba",1986,42000)
+]
 
-
-let ordenNombre = empleados.sort((a, b)=>{
-    return (a.nombre.localeCompare(b.nombre));
+let tabla = document.getElementById("lista-empleados")
+empleados.forEach((empleado) =>{
+    tabla.innerHTML += empleado; 
 });
 
-let ordenApellido = empleados.sort((a, b)=>{
-    return (a.apellido.localeCompare(b.apellido));
-});
+const CASILLAS_VALIDAS = ['Nombre', 'Apellidos', 'Año Nacimiento', 'Sueldo'];
 
-let sueldoMayor = empleados.sort((a, b)=>{
-    return (a.sueldo - b.sueldo);
-});
-
-let ordenNacimiento = empleados.sort((a, b)=>{
-    return (a.nacimiento - b.nacimiento);
-});
-//manda actuar si pulsamos
-function clickCabecera(evento){
-    let cabecera = evento.target;
-    if (comprobarCabeceraValida(cabecera)=='Nombre') {
-        return ordenNombre;
-    } else {
-        
+function principal() {
+    for(let i = 1; i <= 4; i++) {
+        let casilla = document.getElementById(`casilla-${i}`);
+        casilla.addEventListener('click', OrdenarOnClick);
     }
 
 }
-//comprueba si donde has clicado es válido
-function comprobarCabeceraValida(cabecera) {
-    let contenido = cabecera.textContent;
-    return CABECERAS_VALIDAS.includes(contenido);
+
+function comprobarCasillaValida(casilla) {
+    let contenido = casilla.textContent;
+    return CASILLAS_VALIDAS.includes(contenido);
 }
 
+function OrdenarOnClick(event) {
+    let casilla = event.target;
 
-function principal() {
-    let nombre = document.getElementById("nombre");
-    nombre.addEventListener('click', clickCabecera);
-    
-    
-    
-    
-    let tabla =document.getElementById("lista-empleados");
-    
-    empleados.forEach(empleado =>{
-        tabla.innerHTML += empleado;
+    if(comprobarCasillaValida(casilla)) {
+    let columna = casilla.textContent;
+    //el indexOf lo que hace es
+    let indice = CASILLAS_VALIDAS.indexOf(columna);
+
+        empleados.sort((a,b) =>{
+        switch(indice){
+            case 0: //nombre
+            return a.nombre.localeCompare(b.nombre);
+            case 1: // Apellidos
+                return a.apellidos.localeCompare(b.apellidos);
+            case 2: // Año Nacimiento
+                return a.nacimiento - b.nacimiento;
+            case 3: // Sueldo
+                return a.sueldo - b.sueldo;
+            default:
+                return 0;
+        }
     });
-
-
+    
+        tabla.innerHTML = '';
+        empleados.forEach((empleado) => {
+            tabla.innerHTML += empleado;
+    });
+    }
 }
-
 
 principal();
 
